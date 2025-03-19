07786c38f725c0e797ab3b8587972bf49c2149b3
Ivan Chigrinov <chigrinov1998@gmail.com>
2025-03-19 22:21:28 +0300

    update foulder

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const list = document.querySelector('div');
+const modal = document.querySelector('.modal');
+
 async function show() {
     const pack = await fetch('https://dummyjson.com/todos', {
         method: 'GET',
@@ -11,7 +14,54 @@ async function show() {
         par.textContent = arr[i].todo;
         document.querySelector('div').appendChild(par);
 
+        const delete1 = document.createElement('button');
+        const update1 = document.createElement('button');
+
+        delete1.textContent = 'delete';
+        update1.textContent = 'update';
+
+        list.appendChild(delete1);
+        list.appendChild(update1);
+
+        delete1.addEventListener('click', async function () {
+            const deletepack = await fetch(`https://dummyjson.com/todos/${arr[i].id}`, {
+                method: 'DELETE',
+            });
+            list.removeChild(par);
+            list.removeChild(delete1);
+            list.removeChild(update1);
+
+
+        })
+
+
+        update1.addEventListener('click', async function () {
+            modal.style = "display:block";
+            document.querySelector('.put').addEventListener('click', async function(){
+                modal.style = "display:none"
+                let valueinmodal = document.querySelector('.inputupdate').value;
+                const obj2 ={
+                    todo: valueinmodal
+                }
+                const updatepack = await fetch(`https://dummyjson.com/todos/${arr[i].id}`, {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(obj2),
+    
+                });
+    
+                const json3 = await updatepack.json();
+                console.log(json3);
+    
+    
+                par.textContent = valueinmodal;
+            })
+            
+        })
+
     }
+
+
 }
 show();
 
@@ -40,3 +90,6 @@ document.querySelector('button').addEventListener('click', async function () {
 
 
 })
+
+
+
